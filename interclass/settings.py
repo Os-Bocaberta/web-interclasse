33302c931537a47d7825d3f8a232ee6dcf8e5e21
Vineees <marcosvinicius.sm@icloud.com>
2024-09-04 00:48:34 -0300

    🚑️ Solved csrf startup error
    Conflict between names causes error in the CSRF_TRUSTED_ORIGINS parameter

--- interclass/settings.py
+++ interclass/settings.py
@@ -166,7 +166,7 @@ elif (os.getenv("ENVIRONMENT") == 'PROD' or os.getenv("ENVIRONMENT") == 'prod'):
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = [os.getenv('HOST')]
+CSRF_TRUSTED_ORIGINS = ['http://'+os.getenv('HOST'), 'https://'+os.getenv('HOST')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
