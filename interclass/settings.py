1123a8c499bf4ccb36daff6cca462e62c3ef1b72
Vineees <marcosvinicius.sm@icloud.com>
2024-09-04 22:48:33 -0300

    🐛 Solved Redis enviroment bug

--- interclass/settings.py
+++ interclass/settings.py
@@ -51,7 +51,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': [('127.0.0.1', 6379)],
-            "symmetric_encryption_keys": [os.getnv('REDIS_SECRET_KEY')]
+            "symmetric_encryption_keys": [os.getenv('REDIS_SECRET_KEY')]
         }
     }
 }
